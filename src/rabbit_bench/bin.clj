1e9c096ba99268648ab627a0104b980103fd4255
Jonathan Halterman <jhalterman@gmail.com>
2014-01-09 10:15:24 -0800

    Parse command line ints

--- src/rabbit_bench/bin.clj
+++ src/rabbit_bench/bin.clj
@@ -1,4 +1,5 @@
 (ns rabbit-bench.bin
+  (:gen-class)
   (:require [rabbit-bench.core :as core]
             [rabbit-bench.util :refer :all]
             [clojure.string :refer [join]]
@@ -14,11 +15,11 @@
        ["-et" "--exchange-type" "AMQP exchange type" :default "topic"]
        ["-q" "--queue" "AMQP queue to use" :default "test"]
        ["-rk" "--routing-key" "AMQP routing key to use" :default (.toString (UUID/randomUUID))]
-       ["-pml" "--producer-msg-limit" "Message limit for each producer" :default 0]
-       ["-cml" "--consumer-msg-limit" "Message limit for each consumer" :default 0]
-       ["-tl" "--time-limit" "Producer time limit for benchmark" :default 10000]
-       ["-rl" "--rate-limit" "Rate limit of messages / second" :default 0]
-       ["-si" "--sampling-interval" "Sampling interval" :default 1000]
+       ["-pml" "--producer-msg-limit" "Message limit for each producer" :default 0 :parse-fn parse-int]
+       ["-cml" "--consumer-msg-limit" "Message limit for each consumer" :default 0 :parse-fn parse-int]
+       ["-tl" "--time-limit" "Producer time limit for benchmark" :default 10000 :parse-fn parse-int]
+       ["-rl" "--rate-limit" "Rate limit of messages / second" :default 0 :parse-fn parse-int]
+       ["-si" "--sampling-interval" "Sampling interval" :default 1000 :parse-fn parse-int]
        ["-p" "--producer-count" "Producer count" :parse-fn parse-int]
        ["-c" "--consumer-count" "Consumer count" :parse-fn parse-int]
        ["-pc" "--producer-cxn-count" "Producer connection count" :parse-fn parse-int]
